dd139770b34db755b7cfaf29944a126d568d6d3a
yJoaoVictor10 <jvna07@gmail.com>
2025-11-21 11:08:18 -0300

    feat: Colocando as imagens e adicionado a rota /integrantes no main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home/index.tsx'
 import Error from './routes/Error/index.tsx'
 import Sobre from './routes/Sobre/index.tsx'
+import Integrantes from './routes/Integrantes/index.tsx'
 
 const router = createBrowserRouter([
   {path: "/", element: <App/>, errorElement: <Error/>, children:[
   {path:"/", element:<Home/>},
   {path:"/sobre", element:<Sobre/>},
+  {path:"/integrantes", element:<Integrantes/>},
   ]}
 ])
 
